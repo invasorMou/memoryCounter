704a2a0da5807ff6e9bae94cb0806cc9308ef658
Mauricio Scioville <invasormou@gmail.com>
2021-05-22 20:59:34 -0500

    using sw to store some files

--- sw.js
+++ sw.js
@@ -1,5 +1,22 @@
 self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('fox-store').then((cache) => cache.addAll([
+      '/',
+      '/index.html',
+      '/index.js',
+      '/index.css',
+      '/agumon-1.png',
+      '/agumon-2.png',
+      '/gabumon-1.png',
+      '/gabumon-2.png',
+      '/icon/tentomon.png',
+    ])),
+  );
 });
 
 self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
 });
